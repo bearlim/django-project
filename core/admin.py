69478ff7827cc5cf2f26b15355ed2c53f4969d7d
Bearlim <brutchuces@outlook.com>
2022-01-16 16:02:01 -0300

    [Iniciando] Adicionando arquivos arquivos necessários e configurações padrões

---
+++ core/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import tbNOTAFISCAL, tbNOTAFISCALERRO
+
+admin.site.register(tbNOTAFISCAL)
+admin.site.register(tbNOTAFISCALERRO)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,59 @@
+from dataclasses import field
+from operator import mod
+from pyexpat import model
 from django.db import models
 
+class NotaFiscalManager(models.Manager):
+    def pesquisar(self, query):
+        return self.get_queryset().filter(
+            models.Q(dsDiscriminacao__icontains=query) | \
+            models.Q(cdCPFTomador__icontains=query)
+        )
+
 # Create your models here.
+class tbNOTAFISCAL(models.Model):
+    idNFE = models.IntegerField(primary_key=True)
+
+    # Dados de serviço
+    cdServico = models.CharField('Código de Serviço',max_length = 150)
+    cdTributacao = models.CharField('Código de Tributação', max_length = 150)
+    dsDiscriminacao = models.CharField(max_length = 150)
+    cdCNAE = models.CharField('Código CNAE',max_length = 150, blank=True)    
+    vrServico = models.FloatField('Valor do Serviço')
+    vrLiquido = models.FloatField('Valor líquido do Serviço')
+
+    # Dados do prestador
+    cdCNPJPrestador = models.CharField('CNPJ do Prestador',max_length = 255)
+
+    #Dados do Tomador    
+    cdCPFTomador = models.CharField('CPF do Tomador', max_length = 255)
+    cdCEPTomador = models.CharField('CEP do Tomador', max_length = 150)
+    dsTipoLogradouroTomador = models.CharField('Tipo logradouro do Tomador', max_length = 50)
+    dsEnderecoTomador = models.CharField('Endereço do Tomador', max_length = 255)
+    dsTipoBairroTomador = models.CharField('Tipo do bairro do Tomador', max_length = 100)
+    cdCidadeTomador = models.CharField(max_length = 150)
+    sgEstadoTomador = models.CharField(max_length = 50)
+    nrEnderecoTomador = models.IntegerField()    
+    dsBairroTomador = models.CharField(max_length = 150) 
+    urlPDF = models.CharField(max_length = 255, null=True, blank=True)
+    urlXML = models.CharField(max_length = 255, null=True, blank=True)    
+    
+    #Dados da nota
+    dtEmissao = models.DateTimeField(auto_now_add=True, null=True)
+    dsStatus = models.CharField(max_length=100, null=True)
+    flErro = models.BinaryField(null=True)
+
+    objects = NotaFiscalManager()
+
+    def __str__(self) -> str:
+        return self.dsDiscriminacao
+
+class tbNOTAFISCALERRO(models.Model):
+    idErro = models.IntegerField(primary_key=True)
+
+    #Dados do Erro 
+    idNFE = models.IntegerField()
+    dsErro = models.TextField(blank=True)
+
+    def __str__(self) -> str:
+        return self.dsErro
