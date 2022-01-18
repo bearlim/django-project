030344c2a0ef84a91d80fbcb7a47b3dceb7880cc
Bearlim <brutchuces@outlook.com>
2022-01-17 22:05:02 -0300

    [Atualização] Adicionando novos cruds e realizando alterações no front end

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('gerarNota/', views.gerarNota, name='gerarNota'),
+    path('notasGeradas/', views.notasGeradas, name='notasGeradas'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,27 @@
+from multiprocessing import context
+from re import template
 from django.http import HttpResponse
 from django.shortcuts import render
 
+# Importando model
+from .models import tbNOTAFISCAL as NF
+
 def index(request):
     return render(request, 'index.html')
-    
+
+# Função irá trazer modal para realizar a emissão de notas
+def gerarNota(request):
+    return render(request, 'crudGerarNotas.html')
+
+# Função irá puxar todas as notas que estão salvas no banco de dados
+def notasGeradas(request):
+    # Buscando as notas
+    notasGeradas = NF.objects.all()
+    # Definindo o nome do template
+    template_name = 'crudNotasGeradas.html'
+    # Adicionando contexto
+    context = {
+        'notasGeradas': notasGeradas
+    }
+    # Retornando os parâmetros do Request
+    return render(request, template_name, context)
