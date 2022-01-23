d4adbe822585c4e104e61f7a8f26e23a9ed65372
Bearlim <brutchuces@outlook.com>
2022-01-23 20:10:48 -0300

    [Atualização] Atualizado modelo de json

--- core/control.py
+++ core/control.py
@@ -6,16 +6,16 @@ urlAPI = "https://api.sandbox.plugnotas.com.br/nfse"
 headerAPI = {'x-api-key': '2da392a6-79d2-4304-a8b7-959572c7e44d'}
 
 def Gerarjson(NFS):
-    json = {
+    jsonEnviar = [{
         "idIntegracao": NFS.idNFE,
-        "prestador": [
+        "prestador": 
             {"cpfCnpj": NFS.cdCNPJPrestador}
-        ],
-        "tomador": [
+        ,
+        "tomador": 
             {
                 "cpfCnpj": NFS.cdCPFTomador,
                 "razaoSocial": NFS.nmTomador,
-                "endereco": [
+                "endereco": 
                     {
                         "descricaoCidade": NFS.cdCidadeTomador,
                         "cep": NFS.cdCEPTomador,
@@ -27,34 +27,30 @@ def Gerarjson(NFS):
                         "numero": NFS.nrEnderecoTomador,
                         "bairro": NFS.dsBairroTomador
                     }
-                ]
-            }
-        ],
+            },
         "servico": [{
             "codigo": NFS.cdServico,
             "codigoTributacao": NFS.cdTributacao,
             "discriminacao": NFS.dsDiscriminacao,
             "cnae": NFS.cdCNAE,
-            "iss": [{
+            "iss": {
                 "tipoTributacao": 7,
                 "exigibilidade": 1,
                 "aliquota": 3
-            }],
-            "valor": [{
+            },
+            "valor": {
                 "servico": NFS.vrServico,
                 "liquido": NFS.vrLiquido
-            }]
+            }
         }]
-
-    }
-    jsonFormatado = json.dumps(json)
-    return jsonFormatado
-
+    }]
+    jsonFormatador = json.dumps(jsonEnviar)
+    return jsonFormatador
 
 
 def enviarJson(jsonFormatado):
-    response = requests.post(urlAPI, jsonFormatado, headers=headerAPI)
-    jsonRetornoFormatado = json.loads(response)
+    response = requests.post(urlAPI, json=jsonFormatado, headers=headerAPI)
+    jsonRetornoFormatado = json.loads(response.content)
 
     idRetorno = jsonRetornoFormatado['documents']['id']
 
