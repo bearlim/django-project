ccd185b055901f796eddbb68c6dd380260aa1974
Bearlim <brutchuces@outlook.com>
2022-01-25 22:47:23 -0300

    [Atualização] Novas funções

--- core/control.py
+++ core/control.py
@@ -1,23 +1,13 @@
 from django.http import response
-import requests
 import json
 import urllib3
+from django.conf import settings
+import os
 
 urlAPI = "https://api.sandbox.plugnotas.com.br/nfse"
 headerAPI = {'x-api-key': '2da392a6-79d2-4304-a8b7-959572c7e44d', 'Content-Type': 'application/json'}
+http = urllib3.PoolManager()
 
-class JsonRetorno:
-    
-    def __init__(self, documents=None, message=None, protocol=None, erro=None, data=None):
-        self.idRetorno = documents[0]['id']
-        self.message = message
-        self.protocol = protocol
-    
-    @classmethod
-    def deserialize_json(cls, json_string):
-        json_dict = json.loads(json_string)
-        return cls(**json_string)
-    
 
 def Gerarjson(NFS):
     jsonEnviar = [
@@ -66,18 +56,6 @@ def Gerarjson(NFS):
 
 
 def enviarJson(jsonFormatado):
-    response = requests.post(urlAPI, json=jsonFormatado, headers=headerAPI)
-    try:
-        jsonRetornoFormatado = JsonRetorno.deserialize_json(response.content)
-    except:
-        pass    
-
-    idRetorno = jsonRetornoFormatado.idRetorno
-
-    return idRetorno
-
-def enviarJsonV2(jsonFormatado):
-    http = urllib3.PoolManager()
     r = http.request(
         "POST", 
         urlAPI,
@@ -85,11 +63,52 @@ def enviarJsonV2(jsonFormatado):
         headers=headerAPI
         )
 
-    jsonRetornoFormatado = JsonRetorno.deserialize_json(r.data.decode('utf-8'))
-
-    idRetorno = jsonRetornoFormatado.idRetorno
+    json_obj = json.loads(r.data)
+    idRetorno = json_obj.get("documents")[0].get("id")
+    
+    return idRetorno
 
 
 def inserirIdRetorno(idRetorno, NFS):
     NFS.idRetorno = idRetorno
     NFS.save()
+
+
+def verificarStatusNota(idRetorno):
+    r = http.request(
+        "GET", 
+        urlAPI + '/' + idRetorno,
+        headers=headerAPI
+    )
+
+    json_obj = json.loads(r.data)
+    dsStatus = json_obj.get('status')
+
+    return dsStatus
+
+
+def getPDF(idRetorno):
+    direct = str(settings.BASE_DIR) + '/pdfNotas/'
+    urlPDF = f"{direct}{idRetorno}.pdf"
+    url = ""
+    
+    if os.path.isdir(direct):
+        r = http.request(
+            "GET",
+            f"{urlAPI}/pdf/{idRetorno}",
+            headers=headerAPI
+        )
+        with open(urlPDF, "wb") as pdf:
+            pdf.write(r.data)
+        return urlPDF
+    else: 
+        os.mkdir(f"{settings.BASE_DIR}/pdfNotas")
+        r = http.request(
+            "GET",
+            f"{urlAPI}/pdf/{idRetorno}",
+            headers=headerAPI
+        )
+        with open(urlPDF, "wb") as pdf:
+            pdf.write(r.data)
+        return urlPDF
+
